de6c6520ee35ff88adfec5842b1622ba2c0586e9
beecycles <joan.meiners@gmail.com>
2018-08-29 23:40:53 -0500

    more exploration

--- Emissions_script.R
+++ Emissions_script.R
@@ -25,8 +25,6 @@ dim(Current)
 sort(table(Current$RESPONSIBLE_STATE))
 sort(table(Current$LOCATION_STATE))
 head(sort(table(Current$BODY_OF_WATER), decreasing = TRUE), 20)
-table(Current$NUMBER_EVACUATED, Current$LOCATION_STATE)
-table(subset(Current, LOCATION_STATE == "LA")$NUMBER_EVACUATED)
 
 # look at how many were evacuated per state
 Evacuations = Current %>%
@@ -37,3 +35,50 @@ Evacuations = Current %>%
     TOTAL_EVACUATED = sum(NUMBER_EVACUATED, na.rm = TRUE))
 View(Evacuations)
 
+# Number injured
+Injured = Current %>%
+  group_by(LOCATION_STATE) %>%
+  filter(NUMBER_INJURED > 0) %>%
+  summarise(
+    Spills = n(),
+    TOTAL_INJURED = sum(NUMBER_INJURED, na.rm = TRUE))
+View(Injured)
+
+# Fatalities by state
+Fatalities = Current %>%
+  group_by(LOCATION_STATE) %>%
+  filter(NUMBER_FATALITIES > 0) %>%
+  summarise(
+    Spills = n(),
+    TOTAL_EVACUATED = sum(NUMBER_FATALITIES, na.rm = TRUE))
+View(Fatalities)
+
+# Road closure time by state -- La. not on current 2018 list (wonder if it would be now)
+Road_closure = Current %>%
+  group_by(LOCATION_STATE) %>%
+  filter(ROAD_CLOSURE_TIME > 0) %>%
+  summarise(
+    Spills = n(),
+    TOTAL_ROAD_CLOSURE_TIME = sum(ROAD_CLOSURE_TIME, na.rm = TRUE))
+View(Road_closure)
+
+# Medium description by state -- La has the most spills into water
+medium_desc = filter(Current, MEDIUM_DESC == "WATER") %>%
+  group_by(LOCATION_STATE) %>%
+  summarise(
+    Spills = n())
+View(medium_desc)
+
+# sources of spills into MISSISSIPPI description by state -- La the highest
+mississippi = filter(Current, BODY_OF_WATER == "MISSISSIPPI RIVER") %>%
+  group_by(LOCATION_STATE) %>%
+  summarise(
+    Spills = n())
+View(mississippi)
+
+# sources of spills into MISSISSIPPI description by state -- La BY FAR the highest!
+gulf = filter(Current, BODY_OF_WATER == "GULF OF MEXICO") %>%
+  group_by(LOCATION_STATE) %>%
+  summarise(
+    Spills = n())
+View(gulf)
