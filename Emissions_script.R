5893bf6348e4ed5b19cf4c252582376b38b8aa73
beecycles <joan.meiners@gmail.com>
2018-08-31 01:07:59 -0500

    graphing emissions data by state

--- Emissions_script.R
+++ Emissions_script.R
@@ -8,6 +8,30 @@ setwd("/Users/joanmeiners/Dropbox/NOLA.com/USCG-Emissions/")
 #load libraries
 library(dplyr)
 library(tidyverse)
+library(ggplot2)
 
 # run other yearly scripts first to generate objects to combine
-all_emissions = 
+all_emissions = full_join(sumCY05, sumCY10)
+all_emissions = full_join(all_emissions, sumCY17)
+all_emissions = full_join(all_emissions, sumCY18)
+dim(all_emissions)
+all_emissions = all_emissions[,c(1,14,2:13)]
+unique(all_emissions$Year)
+unique(all_emissions$LOCATION_STATE)
+View(all_emissions)
+
+# prep data for plotting
+all_emissions$Year = as.numeric(all_emissions$Year)
+cols = c("grey","grey","grey","grey","grey","grey","grey","lightblue","grey","grey","grey","grey","grey","darkblue","grey","grey","grey","grey","grey","grey","grey","grey","grey","red","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","darkorange","grey","grey","grey","grey","grey","grey","grey","grey","grey")
+
+# plot trend in spills in LA over time compared to state average
+quartz(height = 6, width = 10)
+ggplot(all_emissions, aes(x = Year, y = TOTAL_Spills, colour = LOCATION_STATE)) +
+  geom_point() + 
+  geom_smooth(method = "lm", color = "grey") +
+  xlab("Year") + ylab("Number of Spills") +
+  theme(axis.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=15)) +
+  theme(axis.text = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=10)) +
+  scale_x_continuous(breaks=c(2005, 2010, 2017, 2018)) +
+  scale_y_continuous(labels = function(x) paste0(scales::comma(x))) +
+  scale_color_manual(values=cols)
